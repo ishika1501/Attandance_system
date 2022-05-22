a8ad6d67d3d52a1d682dae9e9005f08ed519841e
ishika <ishika20_ug@cse.nits.ac.in>
2022-05-22 14:51:09 +0530

    Home page GUI changed

--- main.py
+++ main.py
@@ -9,91 +9,91 @@ from face_recognition import Face_Recognition
 
 
 class Face_recognition_system:
-    def __init__(self,root):
+    def __init__(self,root):       #Constructor
         self.root=root
         self.root.geometry("1530x790+0+0")
         self.root.title("face recognisation system")
         
-        #------------------background image-----------------
-        img3=Image.open(r"C:\Users\my pc\Desktop\current_project\images\background.jpg")
-        img3=img3.resize((500,130),Image.ANTIALIAS)
+        #--------------------background image--------------------
+        img3=Image.open(r"C:\Users\my pc\Desktop\current_project\images\abc.jpeg")
+        img3=img3.resize((1530,1170),Image.ANTIALIAS)
         self.photoimg3=ImageTk.PhotoImage(img3)
 
         bg_img=Label(self.root,image=self.photoimg3)
-        bg_img.place(x=0,y=0,width=1530,height=1540)
+        bg_img.place(x=0,y=0,width=1530,height=1170)
 
 
-        #-------------background label-----------------
-        title_lbl=Label(bg_img,text="FACE RECOGNISATION ATTANDANCE SYSTEM", font=("times new roman",35,"bold"),bg="white",fg="red")
-        title_lbl.place(x=0,y=0,width=1530,height=45)
+        #--------------------background label--------------------
+        title_lbl=Label(bg_img,text="FACE RECOGNISATION ATTANDANCE SYSTEM", font=("Arial",25,"bold"),bg="white",fg="grey")
+        title_lbl.place(x=0,y=0,width=1530,height=50)
 
 
 
-        #-------------student button------------------
-        img4=Image.open(r"C:\Users\my pc\Desktop\current_project\images\square1.jpg")
+        #-------------------student button---------------------
+        img4=Image.open(r"C:\Users\my pc\Desktop\current_project\images\sq.jpg")
         img4=img4.resize((1530,710),Image.ANTIALIAS)
         self.photoimg4=ImageTk.PhotoImage(img4)
 
         b1=Button(bg_img,image=self.photoimg4,command=self.student_details,cursor="hand2")
         b1.place(x=200,y=100,width=220,height=220)
 
-        b1_1=Button(bg_img,text="Student Details",command=self.student_details,cursor="hand2",font=("times new roman",15,"bold"),bg="white",fg="red")
+        b1_1=Button(bg_img,text="Student Details",command=self.student_details,cursor="hand2",font=("Arial",15,"bold"),bg="white",fg="grey")
         b1_1.place(x=200,y=300,width=220,height=40)
 
 
-        #-------------detect face------------------
-        img5=Image.open(r"C:\Users\my pc\Desktop\current_project\images\square1.jpg")
+        #-----------------------detect face----------------------
+        img5=Image.open(r"C:\Users\my pc\Desktop\current_project\images\sq.jpg")
         img5=img5.resize((1530,710),Image.ANTIALIAS)
         self.photoimg5=ImageTk.PhotoImage(img5)
 
         b1=Button(bg_img,image=self.photoimg5,cursor="hand2",command=self.face_data)
         b1.place(x=500,y=100,width=220,height=220)
 
-        b1_1=Button(bg_img,text="Face detector",cursor="hand2",command=self.face_data,font=("times new roman",15,"bold"),bg="white",fg="red")
+        b1_1=Button(bg_img,text="Face detector",cursor="hand2",command=self.face_data,font=("Arial",15,"bold"),bg="white",fg="grey")
         b1_1.place(x=500,y=300,width=220,height=40)
 
-        #-------------Attandance face button------------------
-        img6=Image.open(r"C:\Users\my pc\Desktop\current_project\images\square1.jpg")
-        img6=img6.resize((1530,710),Image.ANTIALIAS)
+        #---------------------Attandance face button---------------------
+        img6=Image.open(r"C:\Users\my pc\Desktop\current_project\images\sq.jpg")
+        img6=img6.resize((1530,510),Image.ANTIALIAS)
         self.photoimg6=ImageTk.PhotoImage(img6)
 
         b1=Button(bg_img,image=self.photoimg6)
         b1.place(x=800,y=100,width=220,height=220)
 
-        b1_1=Button(bg_img,text="Attandance",cursor="hand2",font=("times new roman",15,"bold"),bg="white",fg="red")
+        b1_1=Button(bg_img,text="Attandance",cursor="hand2",font=("Arial",15,"bold"),bg="white",fg="grey")
         b1_1.place(x=800,y=300,width=220,height=40)
 
-        #-------------Train data button------------------
-        img8=Image.open(r"C:\Users\my pc\Desktop\current_project\images\square1.jpg")
+        #---------------------Train data button-----------------------
+        img8=Image.open(r"C:\Users\my pc\Desktop\current_project\images\sq.jpg")
         img8=img8.resize((1530,710),Image.ANTIALIAS)
         self.photoimg8=ImageTk.PhotoImage(img8)
 
         b1=Button(bg_img,image=self.photoimg8,cursor="hand2",command=self.train_data)
         b1.place(x=200,y=380,width=220,height=220)
 
-        b1_1=Button(bg_img,text="Train Data",cursor="hand2",command=self.train_data,font=("times new roman",15,"bold"),bg="white",fg="red")
-        b1_1.place(x=200,y=550,width=220,height=40)
+        b1_1=Button(bg_img,text="Train Data",cursor="hand2",command=self.train_data,font=("Arial",15,"bold"),bg="white",fg="grey")
+        b1_1.place(x=200,y=580,width=220,height=40)
 
-        #-------------Photo face button------------------
-        img9=Image.open(r"C:\Users\my pc\Desktop\current_project\images\square1.jpg")
+        #--------------------Photo face button----------------------
+        img9=Image.open(r"C:\Users\my pc\Desktop\current_project\images\sq.jpg")
         img9=img9.resize((1530,710),Image.ANTIALIAS)
         self.photoimg9=ImageTk.PhotoImage(img9)
 
         b1=Button(bg_img,image=self.photoimg9,cursor="hand2",command=self.open_img)
         b1.place(x=500,y=380,width=220,height=220)
 
-        b1_1=Button(bg_img,text="Photo Face ",cursor="hand2",command=self.open_img,font=("times new roman",15,"bold"),bg="white",fg="red")
+        b1_1=Button(bg_img,text="Photo Face",cursor="hand2",command=self.open_img,font=("Arial",15,"bold"),bg="white",fg="grey")
         b1_1.place(x=500,y=580,width=220,height=40)
 
-        #-------------About Developer button------------------
-        img10=Image.open(r"C:\Users\my pc\Desktop\current_project\images\square1.jpg")
+        #--------------------About Developer button--------------------
+        img10=Image.open(r"C:\Users\my pc\Desktop\current_project\images\sq.jpg")
         img10=img10.resize((1530,710),Image.ANTIALIAS)
         self.photoimg9=ImageTk.PhotoImage(img10)
 
         b1=Button(bg_img,image=self.photoimg9)
         b1.place(x=800,y=380,width=220,height=220)
 
-        b1_1=Button(bg_img,text="Developer",cursor="hand2",font=("times new roman",15,"bold"),bg="white",fg="red")
+        b1_1=Button(bg_img,text="Developer",cursor="hand2",font=("Arial",15,"bold"),bg="white",fg="grey")
         b1_1.place(x=800,y=580,width=220,height=40)
 
     #-------------------image opening--------------------
